29a34f574506dc7582ac3ebe93b5e11b8cb4375d
Pedro <55058149+PedroG022@users.noreply.github.com>
2024-03-27 20:14:34 -0300

    feat: Add basic Farm screen
    * Temporarily use bootstrap js hardcoded url in base.html.twig
    * Update twig.yaml to use bootstrap 5 form theme
    * Create farm form type
    * Create farm controller
    * Pending clever confirmation for operations (edit, add, delete), currently using just basic flash messages
    * Add basic title div on base.html.twig

---
+++ src/Controller/FarmController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Farm;
+use App\Form\FarmType;
+use App\Repository\FarmRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Uid\Uuid;
+
+class FarmController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+    private FarmRepository $farmRepository;
+
+    public function __construct(EntityManagerInterface $entityManager, FarmRepository $farmRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->farmRepository = $farmRepository;
+    }
+
+    # Route used to display the index page of
+    # the farms.
+    #[Route('/farms', name: 'index_farm')]
+    public function index(): Response
+    {
+        $farms = $this->farmRepository->findAll();
+
+        return $this->render('farm/index.html.twig', [
+            'farms' => $farms,
+        ]);
+    }
+
+    # Route used to display the page of
+    # creation of a farm.
+    #[Route('/farms/new', name: 'new_farm')]
+    public function new(Request $request): Response
+    {
+        $target_farm = new Farm();
+        $form = $this->createForm(FarmType::class, $target_farm);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $target_farm = $form->getData();
+
+            if ($this->already_exists($target_farm)) {
+                return new Response('Já existe uma fazenda com este nome!', 400);
+            }
+
+            return $this->persist_and_exit($target_farm);
+        }
+
+        return $this->render('farm/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    # Route used to display the edit page of
+    # the farms.
+    #[Route('/farms/edit/{id}', name: 'edit_farm')]
+    public function edit(Uuid $id, Request $request): Response
+    {
+        $database_farm = $this->farmRepository->findOneById($id);
+        $form = $this->createForm(FarmType::class, $database_farm);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $updated_farm = $form->getData();
+
+            if ($this->already_exists($updated_farm)) {
+                return new Response('Já existe uma fazenda com este nome!', 400);
+            }
+
+            return $this->persist_and_exit($updated_farm);
+        }
+
+        return $this->render('farm/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    # Method used to display the edit page of
+    # the farms.
+    #[Route('/farms/delete/{id}', name: 'delete_farm')]
+    public function delete(Uuid $id): Response
+    {
+        $database_farm = $this->farmRepository->findOneById($id);
+
+        if ($database_farm != null) {
+            $this->entityManager->remove($database_farm);
+            $this->entityManager->flush();
+
+        }
+
+        return $this->redirectToRoute('index_farm');
+    }
+
+    # Function used to persist the farm entity to the database,
+    # notify the user of the changes and then redirect the user
+    # to the farm index page.
+    public function persist_and_exit(Farm $farm): Response
+    {
+        # Persist the entity to the database
+        $this->entityManager->persist($farm);
+        $this->entityManager->flush();
+
+        # Add flash message
+        $this->addFlash(
+            'message',
+            'Operação concluída com sucesso!'
+        );
+
+        # Return to the home page
+        return $this->redirectToRoute('index_farm');
+    }
+
+    # Verifies if another farm with the corresponding params already exists
+    # so that when updating a farm, the user can not set its name to something
+    # already present in the database.
+    public function already_exists(Farm $farm): bool
+    {
+        # Verification to see if there is already a farm with the same name
+        $database_farm = $this->farmRepository->findOneByName($farm->getName());
+
+        if ($database_farm != null && $database_farm->getId() !== $farm->getId()) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/Form/FarmType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Farm;
+use App\Entity\Veterinarian;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FarmType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nome da fazenda:',
+                'attr' => ['placeholder' => 'Insira o nome da fazenda']
+            ])
+            ->add('hectares', NumberType::class, [
+                'label' => 'Hectáres:',
+                'attr' => ['placeholder' => 'Insira o tamanho da fazenda, em hectáres']
+            ])
+            ->add('responsible', TextType::class, [
+                'label' => 'Responsável:',
+                'attr' => ['placeholder' => 'Insira o nome do responsável pela fazenda']
+            ])
+            ->add('veterinarians', EntityType::class, [
+                'class' => Veterinarian::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+                'label' => 'Veterinários:',
+                'required' => false])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Cadastrar Fazenda',
+                'attr' => [
+                    'class' => 'btn-primary w-100'
+                ]]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Farm::class,
+        ]);
+    }
+}
